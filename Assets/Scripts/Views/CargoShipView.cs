a39992099947cb1c40c8c9b985cd54bf57f66a47
Ozan Ust <ozanust9@gmail.com>
2026-04-24 00:51:35 +0300

    Ship rotation

--- Assets/Scripts/Views/CargoShipView.cs
+++ Assets/Scripts/Views/CargoShipView.cs
@@ -56,6 +56,19 @@ public class CargoShipView : MonoBehaviour
 	private void Update()
 	{
 		this.transform.position += (currentTarget - transform.position).normalized * shipSpeed * Time.deltaTime * 5;
+		
+		Vector3 direction = (currentTarget - transform.position).normalized;
+
+		transform.position += direction * shipSpeed * Time.deltaTime * 5f;
+
+		float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+		Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
+
+		transform.rotation = Quaternion.RotateTowards(
+			transform.rotation,
+			targetRotation,
+			180f * Time.deltaTime * 10 // turn speed
+		);
 	}
 
 	private void OnPlanetUpdated(PlanetUpdatedSignal signal)
